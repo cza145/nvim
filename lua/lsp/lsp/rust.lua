60a947b354c19fe2b70a2d82b8e48b19dad4243f
cza145 <zyh1032712227@outlook.com>
2022-06-11 21:57:06 +0800

    update

--- lua/lsp/lsp/rust.lua
+++ lua/lsp/lsp/rust.lua
@@ -4,7 +4,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-require('lsp.config').rust_analyzer.setup({
+require('lspconfig').rust_analyzer.setup({
   cmd = { global.lsp.rust },
   filetypes = { "rust" },
   settings = {
